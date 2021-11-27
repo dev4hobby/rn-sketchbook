9e334a00c955af5778b35a803e41880e9f7e673d
d3fau1t <aciddust20@gmail.com>
2021-11-27 16:10:26 +0900

    :lipstick: Update stylesheet
    good for you

---
+++ styles/loading.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+      //앱의 배경 색
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor: '#fdc453',
+  },
+  title: {
+      fontSize:20,
+      fontWeight:'700'
+  }
+
+})
+
+export default styles;
